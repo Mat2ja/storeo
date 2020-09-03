fc843d4490fe38188d22d899293bebfeb66bfcad
Matija Osrecki <matija.osrecki@gmail.com>
2020-09-03 21:53:43 +0200

    added product edit page :sparkles:

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -7,13 +7,12 @@ const { handleErrors, requireAuth } = require('./middlewares');
 const productsRepo = require('../../repositories/products');
 const productsNewTemplate = require('../../views/admin/products/new');
 const productsIndexTemplate = require('../../views/admin/products/index');
+const productsEditTemplate = require('../../views/admin/products/edit');
 const { requireTitle, requirePrice } = require('./validators');
 
 
 const router = express.Router();
 const upload = multer({ storage: multer.memoryStorage() })
-// could've also defined middleware here I think (maybe not cos its not used in all three idk)
-// router.use(upload.single('image'));
 
 // list out all products to admin
 router.get(
@@ -43,6 +42,46 @@ router.post(
         const image = req.file.buffer.toString('base64');
         const { title, price } = req.body;
         await productsRepo.create({ title, price, image })
+
+        res.redirect('/admin/products')
+    }
+);
+
+router.get(
+    // :id is a wildcard
+    '/admin/products/:id/edit',
+    requireAuth,
+    async (req, res) => {
+        // get id from URL
+        const product = await productsRepo.getOne(req.params.id);
+
+        if (!product) {
+            return res.send('Product not found')
+        }
+
+        res.send(productsEditTemplate({ product }))
+    }
+)
+
+router.post(
+    '/admin/products/:id/edit',
+    requireAuth,
+    upload.single('image'),
+    [requireTitle, requirePrice],
+    handleErrors(productsEditTemplate),
+    async (req, res) => {
+        const changes = req.body;
+        console.log(req.body);
+
+        if (req.file) {
+            changes.image = req.file.buffer.toString('base64');
+        }
+
+        try {
+            await productsRepo.update(req.params.id, changes)
+        } catch (error) {
+            return res.send('Could not find item')
+        }
 
         res.redirect('/admin/products')
     }
