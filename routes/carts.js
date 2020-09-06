bc17e98adffb89f66fee56bbfd81163499a7298e
Matija Osrecki <matija.osrecki@gmail.com>
2020-09-06 19:32:03 +0200

    styled the cart and calculated total :sparkles:

--- routes/carts.js
+++ routes/carts.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cartsRepo = require('../repositories/carts');
-
+const productsRepo = require('../repositories/products');
+const cartShowTemplate = require('../views/carts/show');
 const router = express.Router();
 
 // Receive a post request to add an item to cart
@@ -28,7 +29,7 @@ router.post(
             // add new product to item array
             cart.items.push({ id: req.body.productId, quantity: 1 })
         }
-         
+
         await cartsRepo.update(cart.id, {
             items: cart.items
         })
@@ -38,6 +39,28 @@ router.post(
     })
 
 // Receive a get request to show all items in cart
+router.get(
+    '/cart',
+    async (req, res) => {
+        if (!req.session.cartId) {
+            // if cart for the user doesnt exits, return to products
+            //todo build empty cart page
+            return res.redirect('/')
+        }
+
+        const cart = await cartsRepo.getOne(req.session.cartId);
+
+        for (let item of cart.items) {
+            // get product from the product id in the cart
+            const product = await productsRepo.getOne(item.id);
+
+            // temporarily add whole product to cart
+            item.product = product;
+        }
+
+        res.send(cartShowTemplate({ items: cart.items }))
+    }
+)
 
 // Receive a post request to delete an item from cart
 
